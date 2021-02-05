a09f395d2184f4e8a8ab4e3357e5e027e7de92ec
Abby Brown <abby_brown3@baylor.edu>
2021-02-05 10:00:32 -0600

    Still working

--- DICTIONARY HW 1.py
+++ DICTIONARY HW 1.py
@@ -1,24 +1,36 @@
 # START#
 # This program turns a text into a dict#
 
-text_dictionary = {}
-
 
 def main():
+    text_dictionary = {}
+
     infile = open("text.txt", "r")
-    file_contents = str(infile.read())
-    split_it_up(file_contents)
+    file_contents = infile.read()
+    clean_string = strip_string(file_contents)
+    text_dictionary = split_it_up(clean_string, text_dictionary)
 
-    print(f'{"WORD":<12}COUNT')
+    # print(f'{"WORD":<12}COUNT')
+
+    for word, count in sorted(text_dictionary.items()):
+        print(word, count)
 
 
-def split_it_up(file_contents):
-    for word in file_contents.split():
+def strip_string(file_contents):
+    undesired = ["'", '"', ",", ".", "!", ":", ";", "#", "@", "?"]
+    str_check = file_contents.upper()
+    for x in undesired:
+        clean_string = str_check.replace(x, " ")
+    print(clean_string)
+    return clean_string
 
+
+def split_it_up(clean_string, text_dictionary):
+    for word in clean_string.split():
         if word in text_dictionary:
             text_dictionary[word] += 1
-    else:
-        text_dictionary[word] = 1
+        else:
+            text_dictionary[word] = 1
 
     return text_dictionary
 
