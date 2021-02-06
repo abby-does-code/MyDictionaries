d25f5679f70ea5cda72f438b2aabbca5a8945ca2
Abby Brown <abby_brown3@baylor.edu>
2021-02-05 18:29:34 -0600

    Done; turned in at 6:30 after working for HOURS. HW 2 doesn't work.

--- HW_2_25.py
+++ HW_2_25.py
@@ -1,12 +1,24 @@
-number_of_wins = {}
+###START###
 
+##I'm very sorry this code doesn't work, but I gave it my best shot!##
 
 def main():
+    year_won = {}
+    number_of_wins = {}
+    year = int(input("What year do you wanna check? "))
+
     infile = open("worldserieswinners.txt", "r")
-    team_num_wins(infile)
+    team_num_wins(infile, number_of_wins)
+    assign_years(infile, year_won)
+
+    if year == 1904 or year == 1944:
+        print("Sorry, the World Series wasn't played that year.")
+    else:
+        if year in year_won:
+            print(number_of_wins.popitem(), "won", year_won.pop(year))
 
 
-def team_num_wins(infile):
+def team_num_wins(infile, number_of_wins):
 
     winners_txt = infile.readlines()
     for line in winners_txt:
@@ -14,13 +26,18 @@ def team_num_wins(infile):
             number_of_wins[line] += 1
         else:
             number_of_wins[line] = 1
-    print(number_of_wins, end="")
 
 
-def assign_years(infile):
-    line_number = 1903
-    for line in infile.readlines():
-        line_number += 1
+##This part COMPLETELY stumped me, Dr. B! I found a github article that
+##walked through someone's solution to the problem, but I wanted to
+##make it work for myself; I'm pretty sure it's wrong, ha! :) ##
+
+
+def assign_years(infile, year_won):
+    winner_txt = infile.readline().rstrip("\n")
+    for line in winner_txt:
+        for i in range(1903, 2008):
+            year_won[i] = 
 
 
 main()
